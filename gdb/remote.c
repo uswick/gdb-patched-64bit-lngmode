c4ccef200e364f93b937911e11a50d67e433b559
uswick <mastershield2007@gmail.com>
2017-03-26 13:13:13 -0400

    patching remote for g packet too long

--- gdb/remote.c
+++ gdb/remote.c
@@ -7147,9 +7147,28 @@ process_g_packet (struct regcache *regcache)
   buf_len = strlen (rs->buf);
 
   /* Further sanity checks, with knowledge of the architecture.  */
-  if (buf_len > 2 * rsa->sizeof_g_packet)
-    error (_("Remote 'g' packet reply is too long: %s"), rs->buf);
+  /*if (buf_len > 2 * rsa->sizeof_g_packet)*/
+    /*error (_("Remote 'g' packet reply is too long: %s"), rs->buf);*/
 
+  if (buf_len > 2 * rsa->sizeof_g_packet)
+    {
+      rsa->sizeof_g_packet = buf_len ;
+
+      for (i = 0; i < gdbarch_num_regs (gdbarch); i++)
+        {
+          if (rsa->regs[i].pnum == -1)
+            continue;
+
+          if (rsa->regs[i].offset >= rsa->sizeof_g_packet)
+            rsa->regs[i].in_g_packet = 0;
+          else
+            rsa->regs[i].in_g_packet = 1;
+        }
+
+      // HACKFIX: Make sure at least the lower half of EIP is set correctly, so the proper
+      // breakpoint is recognized (and triggered).
+      rsa->regs[8].offset = 16*8;
+    }
   /* Save the size of the packet sent to us by the target.  It is used
      as a heuristic when determining the max size of packets that the
      target can safely receive.  */
